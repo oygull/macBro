4fdcd2c774ed5ba18f1e1676cad01fca13884faa
oygull <oygulkhom3008@gmail.com>
2022-04-11 09:41:06 +0500

    changes

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let arr = [
+  {
+    id:1,
+    color: 'gold',
+    ram: '8GB',
+    memory: ['256GB','512GB'],
+    price: '12 497 000'
+  },
+  {
+    id:2,
+    color: 'silver',
+    ram: '8GB',
+    memory: ['256GB','512GB'],
+    price: '12 497 000'
+  },
+  {
+    id:3,
+    color: 'silver',
+    ram: '16GB',
+    memory: ['256GB','512GB','1TB'],
+    price: '12 497 000'
+  }
+]
